129929f432e4a518bf92b88706812e787480b384
Bryan Castillo <70361389+br-castillo@users.noreply.github.com>
2021-04-29 21:27:22 -0700

    Update splintertutorial.py

--- Tutorials/splintertutorial.py
+++ Tutorials/splintertutorial.py
@@ -2,7 +2,7 @@ from splinter import Browser
 from selenium import webdriver
 
 # define path to chrome driver
-executable_path = {'executable_path':r'C:\Users\Bryan Castillo\Desktop\chromedriver'}
+executable_path = {'executable_path':r'#EXCUTABLE PATH TO SELENIUM WEBDRIVER HERE'}
 
 # Set some default behaviors for browser
 options = webdriver.ChromeOptions()
@@ -24,10 +24,10 @@ browser.visit("https://www.instagram.com/accounts/login/?source=auth_switcher")
 # find the username element
 username_box = browser.find_by_name("username")
 # login
-username_box.fill("yaboibuzzy")
+username_box.fill("IG USERNAME")
 
 # find the password
-password = browser.find_by_name("password").fill("Bcmorales19!")
+password = browser.find_by_name("password").fill("IG PASSWORD")
 
 # submit button
 submit = browser.find_by_class_name("sqdOP  L3NKy   y3zKF     ").first.click()
